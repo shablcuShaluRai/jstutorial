950d00c0c48f24669fdb76c27754e430bc77ec4a
shalurai <shaluraics22@gmail.com>
2017-06-25 12:53:34 +0900

    variables of javascript

---
+++ hello.js
@@ -0,0 +1,16 @@
+//console.log("hello world");
+
+// string variable
+//var x="shalu";
+//console.log(x);
+
+// number variable
+
+var x=22;
+console.log(typeof x);
+
+//concatination of string
+
+//var greeting ="hello ";
+
+//console.log(greeting + " javascript world ");
