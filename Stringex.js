fa0787e164ebd6d3d447fa8ce4f55eee2c229947
shalurai <shaluraics22@gmail.com>
2017-06-25 15:01:10 +0900

    String Index

--- Stringex.js
+++ Stringex.js
@@ -7,10 +7,28 @@
 
 //Escaping characters \"....\"
 
-console.log("hey! \" you can do it\"");
+//console.log("hey! \" you can do it\"");
 //it will print in single quote 'i can learn it'.
-console.log("hey!\'i can learn it \'");
+//console.log("hey!\'i can learn it \'");
 
 
 //"The file located at "C:\\Desktop\My Documents\Roster\names.txt" contains the names on the roster."
-console.log("The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster.");
+//console.log("The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster.");
+
+
+////comparing string:it is case sensitive
+//var x="Yes";
+//var y="yes";
+//console.log(x==y);
+
+
+//string index
+var index=0;
+var str="djfklfairdkfrjinfifnghgkg";
+var len=str.length;
+console.log("total lenght of string "+len);
+while (index<len) {
+  console.log(index,str[index]);
+  index++;
+
+}
