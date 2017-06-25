1bab5d111160734cd22050cfcae3e3a9350972ab
shalurai <shaluraics22@gmail.com>
2017-06-25 14:36:43 +0900

    Escaping characters

--- Stringex.js
+++ Stringex.js
@@ -2,5 +2,15 @@
 //javascript string
 
 //declaraing string
-var name="shalu";
-console.log(name);
+//var name="shalu";
+//console.log(name);
+
+//Escaping characters \"....\"
+
+console.log("hey! \" you can do it\"");
+//it will print in single quote 'i can learn it'.
+console.log("hey!\'i can learn it \'");
+
+
+//"The file located at "C:\\Desktop\My Documents\Roster\names.txt" contains the names on the roster."
+console.log("The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster.");
